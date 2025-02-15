60d6657edc8a90ffd162da2b8b396784caa1b6d7
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-15 00:56:56 -0800

    complete 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedBlogappUser = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedBlogappUser) {
+      const user = JSON.parse(loggedBlogappUser)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -22,12 +30,18 @@ const App = () => {
         username, password
       })
       console.log('user', user)
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('handleLogin', exception)
     }
+  }
+
+  const handleLogout = async () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -60,7 +74,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
