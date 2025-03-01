1a7c99b67043c1fbb9720cd2dc3bc20dcfb696fe
Joe Hsiao <joe.phsiao@gmail.com>
2025-03-01 15:11:15 -0800

    complete 7.14

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,39 +1,18 @@
-const Users = () => {
-  // const [users, setUsers] = useState([])
+import usersService from '../services/users'
+import { useState, useEffect } from 'react'
 
-  const users = [
-    {
-      "username": "user",
-      "name": "Joe",
-      "blogs": [],
-      "id": "67aef04f0f3d9606d6d3004c"
-    },
-    {
-      "username": "joe.phsiao@gmail.com",
-      "name": "Joe",
-      "blogs": [],
-      "id": "67af7d0be34dd3dc21c920db"
-    },
-    {
-      "username": "abc@gmail.com",
-      "name": "ABC",
-      "id": "67af801856164ba8ca1178a8",
-      "blogs": [
-        {
-          "title": "xxx",
-          "author": "xxx",
-          "url": "xxx",
-          "id": "67b3b295853e772f48f2e488"
-        },
-        {
-          "title": "555",
-          "author": "555",
-          "url": "555",
-          "id": "67b3b33f853e772f48f2e490"
-        },
-      ]
-    }
-  ]
+const Users = () => {
+  const [users, setUsers] = useState([])
+
+  const fetchUsers = async () => {
+    const userList = await usersService.getAll()
+    setUsers(userList)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   return (
     <div>
       <h3>Users</h3>
